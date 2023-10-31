2c199c548671d5c3d99bc374e438fd35668818da
Lmonzon1305 <127111945+Lmonzon1305@users.noreply.github.com>
2023-10-31 08:03:40 -0300

    No volver a modificar
    El programa ya fue presentado y hay que volver a presentarlo mas tarde no toques nada

--- src/main/resources/static/js/registrar.js
+++ src/main/resources/static/js/registrar.js
@@ -1,41 +1,41 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-	
+
 	//registrarUsuarios();
-  
+
 });
 
-async function registrarUsuarios(){
-	
-	let datos={};
-	datos.nombre=document.getElementById('txtNombre').value;
-	datos.apellido=document.getElementById('txtApellido').value;
-	datos.email=document.getElementById('txtEmail').value;
-	datos.telefono=document.getElementById('txtTelefono').value;
-	datos.password=document.getElementById('txtPassword').value;
-	
-	let repetirpassword= document.getElementById('txtPassword').value;
-	
-	if(repetirpassword!=datos.password){
-		
+async function registrarUsuarios() {
+
+	let datos = {};
+	datos.nombre = document.getElementById('txtNombre').value;
+	datos.apellido = document.getElementById('txtApellido').value;
+	datos.email = document.getElementById('txtEmail').value;
+	datos.telefono = document.getElementById('txtTelefono').value;
+	datos.password = document.getElementById('txtPassword').value;
+
+	let repetirpassword = document.getElementById('txtRepetirPassword').value;
+
+	if (repetirpassword !== datos.password) {
+
 		alert("Las contraseñas no son las mismas");
 		return;
 	}
-	
-	const request = await fetch('api/usuarios',{
-		
+
+	const request = await fetch('api/usuarios', {
+
 		method: 'POST',
-		
+
 		headers: {
-			
+
 			'Accept': 'application/json',
-			
+
 			'Content-Type': 'application/json'
 		},
-		body:JSON.stringify(datos)
+		body: JSON.stringify(datos)
 	});
-	
+
 	alert('Usuario registrado con éxito');
-	
-	window.location.href='login.html';
+
+	window.location.href = 'login.html';
 }
